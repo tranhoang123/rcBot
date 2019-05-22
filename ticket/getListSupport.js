2cbca8efaea13aad3df55ed8757acfa9f4f78d69
tranhoang123 <43273634+tranhoang123@users.noreply.github.com>
2019-05-22 22:13:26 +0700

    Update getListSupport.js

--- ticket/getListSupport.js
+++ ticket/getListSupport.js
@@ -9,7 +9,7 @@ var getListSupport = () => {
         request.get({
             url: "http://localhost:8118/api/v1/users",
             headers: {
-                "accesstoken": "1414806da5739e54c50f04a17ae9d67c7cd29ad7" 
+                "accesstoken": "22fd06ffe7f8052d1e8e7eee52e1cfb6eb7b0dce" 
             },
         }, function (err, req, body) {
             if (err) reject(err);
